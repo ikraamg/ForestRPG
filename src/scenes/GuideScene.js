a375ba0c8b771d1c3236d0f7e6a0e2f6e93cd518
Ikraam <ikraamghoor@gmail.com>
2020-09-15 16:18:21 +0200

    Improve instructions

--- src/scenes/GuideScene.js
+++ src/scenes/GuideScene.js
@@ -18,7 +18,25 @@ export default class GuideScene extends Phaser.Scene {
 
   addDisplayElements() {
     this.add.tileSprite(400, 300, 800, 600, 'title-bg');
-    this.add.image(400, 250, 'instructions').setScale(3.5);
-    this.add.image(400, 500, 'enter').setScale(3);
+    this.add.image(400, 225, 'instructions').setScale(3.5);
+    this.add.image(400, 525, 'enter').setScale(3);
+
+    this.add.text(120, 450,
+      'Get 6 kills or more to open the mountain door.', {
+        fontFamily: 'monospace',
+        fontSize: 20,
+        fontStyle: 'bold',
+        color: '#ffffff',
+        align: 'center',
+      });
+
+    this.add.text(20, 415,
+      'Try to quickly kill the most enemies, with the the least arrows.', {
+        fontFamily: 'monospace',
+        fontSize: 20,
+        fontStyle: 'bold',
+        color: '#ffffff',
+        align: 'center',
+      });
   }
 }
