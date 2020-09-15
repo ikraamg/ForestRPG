0edb3ac9d7821f309483e3133167da2fc291c7cc
Ikraam <ikraamghoor@gmail.com>
2020-09-15 19:35:32 +0200

    update tests

--- src/tests/API.test.js
+++ src/tests/API.test.js
@@ -1,15 +1,40 @@
 import { postScore, getScores } from '../api/api';
 
 describe('Post and Get scores from API', () => {
-  it('Post score', () => {
-    postScore('Ikraam', 20).then((data) => {
-      expect(data).toBe('Leaderboard score created correctly.');
+  test('Should save the score to the API', () => {
+    postScore('Ikraam', 10).then(data => {
+      expect(data.result).toBe('Leaderboard score created correctly.');
     });
   });
-  it('Gets scores', () => {
-    getScores().then((data) => {
+  test('Should receive an object from the API', () => {
+    getScores().then(data => {
       expect(typeof data).toBe('object');
-      expect(data.result).toContainEqual('Ikraam');
+    });
+  });
+  test('The object should contain the created user', () => {
+    getScores().then(data => {
+      expect(data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            user: 'Ikraam',
+          }),
+        ]),
+      );
+    }).catch(() => {
+
+    });
+  });
+  test('The object should contain the created score', () => {
+    getScores().then(data => {
+      expect(data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            score: '10',
+          }),
+        ]),
+      );
+    }).catch(() => {
+
     });
   });
 });
